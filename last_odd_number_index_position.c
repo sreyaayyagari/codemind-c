a4ca8be024a57ad4940a3f43e8e45ff100e514a5
sreyaayyagari <97898923+sreyaayyagari@users.noreply.github.com>
2022-05-30 11:22:47 +0530

    #include<stdio.h> int main() {     int n,m,i,a[100];     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         if(a[i]%2!=0)         {             m=i;         }     }     printf("%d",m); }

---
+++ last_odd_number_index_position.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n,m,i,a[100];
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]%2!=0)
+        {
+            m=i;
+        }
+    }
+    printf("%d",m);
+}
