fd35c41b4046b39b3d55268c3e3c062ae52fb494
sreyaayyagari <97898923+sreyaayyagari@users.noreply.github.com>
2022-07-07 11:18:20 +0530

    #include<stdio.h> int main() {     int i,n,a,b,arr[100],s=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {       scanf("%d",&arr[i]);     }     scanf("%d%d",&a,&b);     for(i=0;i<n;i++)     {       if(arr[i]>a && arr[i]>b || arr[i]<a && arr[i]<b)         {          s=s+arr[i];         }     }     {         printf("%d",s);
        }
    }

---
+++ sum_of_elements_not_between_A_and_B.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,a,b,arr[100],s=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+      scanf("%d",&arr[i]);
+    }
+    scanf("%d%d",&a,&b);
+    for(i=0;i<n;i++)
+    {
+      if(arr[i]>a && arr[i]>b || arr[i]<a && arr[i]<b)
+        {
+         s=s+arr[i];
+        }
+    }
+    {
+        printf("%d",s);
+        
+    }
+}
